eb892b54e96481364cc4e482d8227e390985492d
AbhiAtiUttam <Abhi@glocalize.net>
2024-09-06 21:33:10 +0530

    v0 frontend ready

---
+++ src/utils/CalCoordinate.js
@@ -0,0 +1,17 @@
+// function  cordinateX(x){
+//     if(x>350){
+//         return x-350;
+//     }
+//     else{
+//         return 
+//     }
+// }
+
+// function  cordinateY(array){
+
+// }
+
+
+// module.exports={
+//     cordinate
+// }
---
+++ src/walls/Walls.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from 'react-dom/client'
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
+export default function Walls(props) {
+    console.log(props)
+  const meshRef = useRef()
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  const colorMap = useLoader(TextureLoader,"brickTexture.jpg")
+
+  return (
+    <mesh
+    //   {...props}
+      position={props.position}
+      rotation={[0,props.rotation?props.rotation:0,0]}
+      ref={meshRef}>
+      <boxGeometry args={[props.size, 70, 6]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} map={colorMap} />
+    </mesh>
+  )
+}
